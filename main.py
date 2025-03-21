b95a3be88bbcd363274f6fa5af6238b1b81ef820
clown6910clown <tushar.garg.btech2023@sitpune.edu.in>
2025-03-22 01:30:00 +0530

    commit

--- main.py
+++ main.py
@@ -39,10 +39,14 @@ while game_is_on:
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreBoard.reset()
+        snake.reset()
+
+
     for segment in snake.segments:
         if segment == snake.head:
             pass
         elif snake.head.distance(segment) < 10:
             scoreBoard.reset()
+            snake.reset()
 
 screen.exitonclick()
